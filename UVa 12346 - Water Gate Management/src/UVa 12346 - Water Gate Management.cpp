fcb48c6d6b94b382ed04d83e52ae39bd854618a3
Arnold <arnold.koroa@gmail.com>
2013-02-04 22:10:14 +0800

    UVa 12346

---
+++ UVa 12346 - Water Gate Management/src/UVa 12346 - Water Gate Management.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+using namespace std;
+
+int f[20], c[20];
+
+int main() {
+	int n, tc, v, t, count = 0;
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &f[i], &c[i]);
+	}
+
+	scanf("%d", &tc);
+	while (tc--) {
+		count++;
+		scanf("%d %d", &v, &t);
+
+		int minc = 10000000, tf, tcost;
+		for (int s = 0; s < (1 << n) - 1; s++) {
+			tf = tcost = 0;
+			for (int i = 0; i < n; i++) {
+				if (1 & (s >> i)) {
+					tf += f[i];
+					tcost += c[i];
+				}
+			}
+			if (tf * t >= v) {
+				if (tcost < minc) {
+					minc = tcost;
+				}
+			}
+		}
+
+		if (minc < 10000000)
+			printf("Case %d: %d\n", count, minc);
+		else
+			printf("Case %d: IMPOSSIBLE\n", count);
+	}
+
+	return 0;
+}
