d98f53f0e435d00c0518d4cb80d632a2813ff4fd
Arnold <arnold.koroa@gmail.com>
2013-02-05 00:04:11 +0800

    Optimized using bidirectional search and enabled for n<=30 and each number <=1000000000

--- UVa 12455 - Bars/src/UVa 12455 - Bars.cpp
+++ UVa 12455 - Bars/src/UVa 12455 - Bars.cpp
@@ -1,33 +1,47 @@
 #include <cstdio>
+#include <set>
 using namespace std;
 
-int bar[20];
+int bar[30];
 
 int main() {
-	int tc, target, total, n;
+	int tc, n;
+	long long total, target;
+	set<long long> checker;
 
 	scanf("%d", &tc);
 	while (tc--) {
-		scanf("%d\n%d", &target, &n);
+		scanf("%lld\n%d", &target, &n);
+
 		for (int i = 0; i < n; i++) {
 			scanf("%d", &bar[i]);
 		}
 
-		for (int s = 0; s < (1 << n); s++) {
+		int n2 = n / 2, n1 = n - n2;
+		for (int s = 0; s < (1 << n1); s++) {
 			total = 0;
-			for (int i = 0; i < n; i++) {
+			for (int i = 0; i < n1; i++) {
 				if (1 & (s >> i)) {
 					total += bar[i];
 				}
 			}
-			if (total == target)
-				break;
+			checker.insert(total);
 		}
 
-		if (total == target)
-			printf("YES\n");
-		else
-			printf("NO\n");
+		for (int s = 0; s < (1 << n2); s++) {
+			total = 0;
+			for (int i = 0; i < n2; i++) {
+				if (1 & (s >> i)) {
+					total += bar[n1+i];
+				}
+			}
+			if(checker.count(target-total)) break;
+		}
+
+		if(checker.count(target-total)) printf("YES\n");
+		else printf("NO\n");
+
+		checker.clear();
 	}
 
 	return 0;
